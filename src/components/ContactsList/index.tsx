dedcafa48ba6d4958e13928d7b6a2a8b11bb6029
Bryan Aldrin Quinalayo <bryan@pinion.ph>
2023-08-24 22:57:46 +0800

    fix(space): cleanup

--- src/components/ContactsList/index.tsx
+++ src/components/ContactsList/index.tsx
@@ -2,7 +2,6 @@ import { Contact } from "../../types/Contact";
 import ContactCard from "../ContactCard";
 import { faker } from '@faker-js/faker'
 
-
 export default function ContactsList() {
   return (
     <div className="flex flex-col gap-4 my-9 px-6 md:grid md:grid-cols-2 lg:px-12 xl:px-36 2xl:px-80">
