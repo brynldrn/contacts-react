514a09cce6b5097bb6e30911e3b881cc15c58c83
Bryan Aldrin Quinalayo <bryan@pinion.ph>
2023-08-24 22:57:09 +0800

    fix(spacing): give more breathing room on both sides

--- src/components/ContactsList/index.tsx
+++ src/components/ContactsList/index.tsx
@@ -5,7 +5,7 @@ import { faker } from '@faker-js/faker'
 
 export default function ContactsList() {
   return (
-    <div className="flex flex-col gap-4 my-9 px-6 md:grid md:grid-cols-2 lg:px-12 xl:grid-cols-3">
+    <div className="flex flex-col gap-4 my-9 px-6 md:grid md:grid-cols-2 lg:px-12 xl:px-36 2xl:px-80">
       {[...new Array(50)].map((_) => {
         const contactDetails: Contact = {
           avatarUrl: faker.image.avatar(),
